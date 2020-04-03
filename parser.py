1e12dbc109a222e17d5d2cc5eaca093e40fd3f3c
Egor <egor_mixajlichenko@mail.ru>
2020-04-03 22:28:18 +0300

    created list of cities in the world

--- parser.py
+++ parser.py
@@ -1,22 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'http://www.1000mest.ru/cityA'
+URL = 'http://www.1000mest.ru/cityYa'
 HEADERS = {
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \
 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36 OPR/67.0.3575.105',
-	'Accept': '*/*'
+    'Accept': '*/*'
 }
 
 
 def get_html(url, params=None):
-	r = requests.get(url, headers=HEADERS, params=params)
-	return r
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('tr')
+    cities = []
+    for item in items:
+        cities.append({
+            'city': item.find('td').get_text()
+        })
+    return cities
+
+
+def save_file(item, path):
+    with open(path, 'a', encoding='utf-8') as file:
+        file.write(item)
 
 
 def parse():
-	html = get_html(URL)
-	print(html)
+    html = get_html(URL)
+    if html.status_code == 200:
+        cities = get_content(html.text)
+        for i in range(0, len(cities)):
+            save_file(str(cities[i].values())+'\n', 'cities.txt')
+    else:
+        print('Error')
 
 
 parse()
