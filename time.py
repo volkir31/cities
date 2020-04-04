d3cdc2f390d24b03d71fefa12b38f5448ee69ae3
Egor <egor_mixajlichenko@mail.ru>
2020-04-04 20:38:44 +0300

    Таймер

---
+++ time.py
@@ -0,0 +1,11 @@
+import time
+
+
+start = time.time()
+for i in range(1, 900000000000000000000000000000000000000000):
+    limit_time = 60
+    time.sleep(1)
+    if time.time() - start >= limit_time:
+        print('Вы проиграли!\nПопробуйте еще раз')
+    else:
+        print(f'У вас осталось { int(limit_time-(time.time() - start))}, поторопитесь!')
